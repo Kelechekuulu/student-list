1d5b9409ab52120639ad984cfdeee369e31ffdca
Nurpeiil <nurpeiilk99@gmail.com>
2023-05-02 16:54:03 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
 
-function App() {
+ function App() {
+
+  const [data, setData] = useState([])
+  const [newDate, setNewDate] = useState();
+  useEffect(()=>{
+
+  fetch ('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => setData(json));
+  },[]);
+
+  async function fetchData(id){
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    const responseData = await  response.json();
+    setNewDate(responseData);
+  }
+  useEffect(()=>{
+    fetchData()
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Student List</h1>
+      {data.map((el)=><Style onClick ={()=>fetchData(el.id) } key={el.id}>{el.name}</Style>)}
+      {newDate&&<div className="color">
+        <p>{newDate.name}</p>
+        <p>{newDate.id}</p> 
+        <p>{newDate.email}</p>
+        </div>}
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+const Style = styled.div`
+background-color: red;
+margin-top: 5px;
+border: 1px solid;
+width:200px;
+height:40px;
+text-align:center;
+`
+
+
